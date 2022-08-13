73130de215fc7b86be00f04d633f5821d58f3151
welbber <welbervital01@gmail.com>
2022-08-13 12:05:00 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/github/hrworker/entities/Worker.java
@@ -0,0 +1,25 @@
+package com.github.hrworker.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+@Entity
+@Table(name = "tb_worker")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double dailyIncome;
+}
